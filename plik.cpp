c1b7238f8b0ef172230f1063aadcbff7604d05b2
Anna Krupej <z1143038@s0053-05.stud.matinf.uj.edu.pl>
2017-10-25 10:10:42 +0200

    sortowanie boblekowe

--- plik.cpp
+++ plik.cpp
@@ -4,24 +4,34 @@ using namespace std;
 int main()
 {
 	int size=9;
-	int tab[size];
-	
-	
-
-	srand(time(0));
-	for(int i=0;i<size;i++)
-	{
-		tab[i]=(rand()%100);
+	int t[size]={3,5,7,2,65,33,1,9,12};
+	int schowek;
+	int zamiana=0;
+	do
+{
+	int zamiana=0;
+		for (int i=0; i<9; i++){
 		
+		if (t[i]>t[i+1]){
+		
+			zamiana=zamiana+1; 
+			schowek=t[i];
+			t[i]=t[i+1];
+			t[i+1]=schowek;
+		}
 	}
-	int max=tab[0];
-	for(int j=1;j<size;j++){
+}
+				
+		while(zamiana!=0);
+		for (int j=0; j<=5; j++)
+{
+		cout<<t[j]<<" ";
+	}		
+				return 0;
+}
+	
+	
 
-	if(tab[j]>max)
-	max=tab[j];
-}
-	
-	cout<<max<<endl;
 	
 	
 	
@@ -30,4 +40,5 @@ int main()
 	
 	
 	
-}
+	
+

96a132df3b1b777ed0250495803e08aa55e074a6
AnnaKrupej <32893430+AnnaKrupej@users.noreply.github.com>
2017-11-28 22:40:01 +0100

    Create zad5.sh

---
+++ zad5.sh
@@ -0,0 +1,12 @@
+    #!/bin/bash
+    #pierwszy skrypt
+    
+    while [ -f "$1" ]
+    do
+
+       echo "Najczesciej powtarzajace slowa w pliku $1 to:"
+       cat $1 | tr -s ' ' '\n' | sort | uniq -c | sort -r | head -n 3
+      
+	 echo " "
+       shift
+    done
